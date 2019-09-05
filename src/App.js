ca6c3e8f56a5afa1823d4125ca1e3c254021c683
SeungheeLee <1111womyn@naver.com>
2019-09-05 17:39:24 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+import Zoom from 'react-reveal/Zoom';
+import Flip from 'react-reveal/Flip';
+class App extends Component {
+ constructor(props){
+   super(props)
+   this.state={
+     name:" ",
+     age:""
+   }
+ }
+ getData = (e)=>{
+   e.preventDefault()
+   fetch('http://localhost:3001/data')
+   .then(res=>res.json())
+   .then(resData => {
+     console.log(resData)
+     this.setState({
+       name:resData[0].name,
+       age:resData[0].age
+     })
+   }
+   )}
+ postData = (e)=>{
+   e.preventDefault()
+   fetch('http://localhost:3001/data')
+   }
+ render(){
+   return(
+     <div>
+       <Zoom>
+       <center>
+       <Flip>
+         <h3>My Cats List MySQL</h3>
+       </Flip>
+       <form style={{flex:1 , flexDirection:'row'}}>
+         <input
+           type = 'text'
+           id = "name"
+           placeholder = "이름"
+          //  ref ={age=>this.input.age}
+         />
+         <input
+           type = 'text'
+           id = "age"
+           placeholder = "나이"
+          //  ref ={age=>this.input.age}
+         />
+         <button
+         style={{width:'100px', height:'20px'}}
+         onClick={this.postData.bind(this)}
+         >강아지 등록</button>
+         <button
+         style={{width:'100px', height:'20px'}}
+         onClick={this.getData.bind(this)}
+         >강아지 조회</button>
+       </form>
+       </center>
+       </Zoom>
+     <Zoom>
+       <center>
+       <h1><span>{this.state.name}의 나이는 {this.state.age}살</span></h1>
+       </center>
+     </Zoom>
+     </div>
+   )
+ }
 }
-
 export default App;
+
